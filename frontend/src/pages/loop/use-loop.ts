dcc4eb10ba030a995263f5ee5169dfe1ac7a5d31
Arnis Vuskans <ampatspell@gmail.com>
2026-04-04 03:35:44 +0300

    random

--- frontend/src/pages/loop/use-loop.ts
+++ frontend/src/pages/loop/use-loop.ts
@@ -1,7 +1,7 @@
 import { useCamera, type UsedCamera } from "@/lib/camera";
 import { useVideos } from "@/lib/videos";
 import { useWebsocket } from "@/lib/web-socket";
-import { onKeyPressed } from "@vueuse/core";
+import { onKeyStroke } from "@vueuse/core";
 import { computed, shallowRef, watch } from "vue";
 
 const rnd = (min: number, max: number) => {
@@ -9,10 +9,11 @@ const rnd = (min: number, max: number) => {
 };
 
 const createVideo = (opts: { name: string; url: string }) => {
+  const { name } = opts;
   const element = document.createElement("video");
   element.src = opts.url;
 
-  return { type: "video" as const, element };
+  return { type: "video" as const, name, element };
 };
 
 const createCamera = (camera: UsedCamera) => {
@@ -66,6 +67,8 @@ export const useLoop = () => {
 
   const video = shallowRef<LoopVideo>();
 
+  const prev = () => {};
+
   const next = () => {
     const index = rnd(0, videos.all.value.length);
     const next = videos.all.value[index]!;
@@ -75,16 +78,8 @@ export const useLoop = () => {
     }
   };
 
-  onKeyPressed("ArrowLeft", () => {}, { target: document.body });
-
-  onKeyPressed(
-    "ArrowRight",
-    () => {
-      console.log("next");
-      next();
-    },
-    { target: document.body },
-  );
+  onKeyStroke("ArrowLeft", () => prev());
+  onKeyStroke("ArrowRight", () => next());
 
   watch(videos.all, (all) => {
     if (all.length) {
